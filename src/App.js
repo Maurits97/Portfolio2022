fcfef72e27f48571cb05d2fa2647ae21d15796aa
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2022-07-26 15:47:10 +0200

    start header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <div className="grid container">
-        <h1 className="col-l6">Test</h1>
-        <Header />
-      </div>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
+import styles from '../styles/components/Header.module.scss';
+
 function Header() {
   return (
-    <header>
-      <h1>Yoooo</h1>
+    <header className={ styles['header-animation'] }>
+      <div className="grid container">
+        <div className={ `col-12 ${ styles['header-container'] }` }>
+          <span className={ `${ styles['header__start'] }` }>I am an</span>
+          <h1 className={ `${ styles.header }` }>
+            <span className={ `${ styles['header__outline'] }` }>Enthousiastic</span>
+            Front-end developer
+          </h1>
+          <div className={ styles['header__paragraph-wrapper'] }>
+            <p className={`raleway ${ styles['header__paragraph']}`}>with a passion for design.</p>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
