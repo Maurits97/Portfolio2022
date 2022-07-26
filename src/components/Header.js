bec55c44500eaa919dc7086c2e8ae8bef568bf1e
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2022-07-26 21:50:52 +0200

    started with intro

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,15 @@ import styles from '../styles/components/Header.module.scss';
 
 function Header() {
   return (
-    <header className={ styles['header-animation'] }>
-      <div className="grid container">
-        <div className={ `col-12 ${ styles['header-container'] }` }>
-          <span className={ `${ styles['header__start'] }` }>I am an</span>
-          <h1 className={ `${ styles.header }` }>
-            <span className={ `${ styles['header__outline'] }` }>Enthousiastic</span>
-            Front-end developer
-          </h1>
-          <div className={ styles['header__paragraph-wrapper'] }>
-            <p className={`raleway ${ styles['header__paragraph']}`}>with a passion for design.</p>
-          </div>
+    <header className={`grid container ${ styles.header }`}>
+      <div className={ `col-12 ${ styles['header-container'] }` }>
+        <span className={ `${ styles['header__start'] }` }>I am an</span>
+        <h1 className={ `${ styles['header__title'] }` }>
+          <span className={ `${ styles['header__outline'] }` }>Enthousiastic</span>
+          Front-end developer
+        </h1>
+        <div className={ styles['header__paragraph-wrapper'] }>
+          <p className={`raleway ${ styles['header__paragraph']}`}>with a passion for design.</p>
         </div>
       </div>
     </header>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,13 @@
+import styles from '../styles/components/Intro.module.scss';
+
 function Intro() {
   return (
-    <section>
-      <h1>Intro</h1>
+    <section className='grid container'>
+      <div className={`col-12 ${ styles.intro }`}>
+        <p>Hi, my name is Maurits Brouwer, an ethousiastic and creative front-end developer with a passion for design. I make cool digital products and make sure the end-user always has the right experience.</p>
+
+        <p>I studied Communication &amp; Multimedia Design at the Hague University.</p>
+      </div>
     </section>
   )
 }
