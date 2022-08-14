f33e39d6909ba9ef8615d5fa0d9fc3436525955d
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2022-08-14 14:20:50 +0200

    feat(contact+links): Added contact section + started on links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,34 @@
+import { useEffect } from 'react';
+import styles from '../styles/components/Contact.module.scss';
+
+function copyToClipboard(event) {
+  const eventData = event.target.dataset.email;
+  navigator.clipboard.writeText(eventData);
+
+  // User copy confirmation
+  event.target.innerHTML = '&#10003; Copied!';
+  setTimeout(() => {
+    event.target.innerHTML = 'Copy Email';
+  }, 1500)
+}
+
 export default function Contact() {
+  useEffect(() => {
+    const buttonCopy = document.querySelector('.js-button-copy');
+    buttonCopy.addEventListener('click', (event) => {
+      copyToClipboard(event);
+    })
+  });
+
   return (
-    <section className="grid container">
+    <section className={`grid container ${ styles['contact'] }`}>
       <div className="col-12">
-        <h2 className="h2">Contact</h2>
+        <h2 className={`h2 ${ styles['contact__header'] }`}>Contact</h2>
+        <p className={ styles['contact__email'] }>mauritsbrouwer@outlook.com</p>
+        <div className={ styles['contact__button-wrapper'] }>
+          <button className="button button--primary js-button-copy" data-email="mauritsbrouwer@outlook.com">Copy Email</button>
+          <a className="button button--secondary" href="mailto:mauritsbrouwer@outlook.com" target="_blank" rel="noreferrer noopener">Open mail program</a>
+        </div>
       </div>
     </section>
   )
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,8 +1,18 @@
+import links from '../data/links.json'
+import styles from '../styles/components/Links.module.scss';
+
+
 export default function Links() {
+  const linksData = links;
   return (
-    <section className="grid container">
+    <section className={`grid container ${ styles['links'] }`}>
       <div className="col-12">
-        <h2 className="h2">Links</h2>
+        <h2 className={`h2 ${ styles['links__header'] }`}>Links</h2>
+        { linksData.links.map((link, index) => 
+          <div>
+            <a className={`raleway ${ styles['links__link'] }`} href={ link.link } target="_blank" rel="noreferrer noopener" key={ index }>{ link.label }</a>
+          </div>
+        )}
       </div>
     </section>
   )
