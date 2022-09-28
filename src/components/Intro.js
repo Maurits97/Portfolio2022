15444dfde253fa022ce0cd72e854aade5ad0cbba
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2022-09-28 11:51:42 +0200

    stupid typo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ export default function Intro() {
     <section className={`grid container ${ styles.intro }`}>
       <div className='col-12 col-s10 col-m8'>
         <SkewVelocity>
-          <p>Hi, my name is Maurits Brouwer, an ethousiastic and creative front-end developer with a passion for design. I make cool digital products and make sure the end-user always has the right experience.</p>
+          <p>Hi, my name is Maurits Brouwer, an enthousiastic and creative front-end developer with a passion for design. I make cool digital products and make sure the end-user always has the right experience.</p>
 
           <p>I studied Communication &amp; Multimedia Design at The Hague University.</p>
         </SkewVelocity>
