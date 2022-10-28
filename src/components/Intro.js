8a232aec0c1e83ca277d094cd2485a0e503aa680
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2022-10-28 16:02:11 +0200

    job update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,8 +7,8 @@ export default function Intro() {
       <div className='col-12 col-s10 col-m8'>
         <SkewVelocity>
           <p>Hi, my name is Maurits Brouwer, an enthousiastic and creative front-end developer with a passion for design. I make cool digital products and make sure the end-user always has the right experience.</p>
-
           <p>I studied Communication &amp; Multimedia Design at The Hague University.</p>
+          <p>I am currently working at Coolblue in Rotterdam</p>
         </SkewVelocity>
       </div>
     </section>
